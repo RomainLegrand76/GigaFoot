d9545237b051bc3517fa66ffc49c4627bce5bd8a
Vincent JONQUAIS <jonquais.vincent@laposte.net>
2018-06-04 15:54:00 +0200

    Modification style de la page "Inscription"

--- src/Controller/ConnexionController.php
+++ src/Controller/ConnexionController.php
@@ -2,18 +2,60 @@
 
 namespace App\Controller;
 
+
+use App\Entity\Users;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 
 class ConnexionController extends Controller
 {
     /**
      * @Route("/connexion", name="connexion")
      */
-    public function index()
+    public function index(Request $requete)
     {
-        return $this->render('connexion/index.html.twig', [
-            'controller_name' => 'ConnexionController',
-        ]);
+        $connexion = new Users();
+
+        $formulaire = $this -> createFormBuilder($connexion)
+            ->add('use_pseudo')
+            ->add('use_password',PasswordType::class)
+            ->add('Connexion', SubmitType::class)
+            ->getForm();
+
+        $formulaire->handleRequest($requete);
+
+        if ($formulaire->isSubmitted() && $formulaire->isValid())
+        {
+            $connexion = $formulaire->getData();
+
+            $qb = $this->getDoctrine()
+                ->getRepository(Users::class)
+                ->findOneBy(['use_pseudo' => $connexion->getUsePseudo()]);
+
+            if ($connexion->getUsePseudo() === $qb->getUsePseudo()){
+                if(password_verify($connexion->getUsePassword(), $qb->getUsePassword())){
+                    return $this->redirectToRoute('deck');
+                }
+                else{
+                    echo 'Mot de passe invalide';
+                }
+            }
+            else{
+                echo 'Pseudo invalide';
+            }
+
+
+
+
+        }
+
+        return $this->render('connexion/index.html.twig',
+            array(
+                'connexion' => $formulaire->createView()
+            )
+        );
     }
 }
