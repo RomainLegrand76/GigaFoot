93d65bea3807beeb052813466c5c1a7c0c8ab40a
jado <tony76.200@hotmail.fr>
2018-06-18 19:29:46 +0200

    erreur deck

--- src/Controller/DeckController.php
+++ src/Controller/DeckController.php
@@ -6,27 +6,29 @@ use App\Entity\Countries;
 use App\Entity\CourseOf;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
-use Symfony\Component\HttpFoundation\Response;
 
 class DeckController extends Controller
 {
     /**
      * @Route("/deck", name="deck")
      */
-    public function index()
-    {
+     public function index()
+   {
 
-      /*  $country = $this->getDoctrine()
-            ->getRepository(CourseOf::class)
-            ->findOneBy($date);
+       return $this->render('deck/index.html.twig', [
+           'controller_name' => 'DeckController',
+       ]);
+   /*  $country = $this->getDoctrine()
+           ->getRepository(CourseOf::class)
+           ->findOneBy($date);
 
-        $match = $this->getDoctrine()
-            ->getRepository(CourseOf::class)
-            ->findOneByIdJoinedToCategory('2018-06-16 12:00:00');
+       $match = $this->getDoctrine()
+           ->getRepository(CourseOf::class)
+           ->findOneByIdJoinedToCategory('2018-06-16 12:00:00');
 
-        var_dump($match);
+       var_dump($match);
 
-        return $this->render('deck/index.html.twig');*/
+       return $this->render('deck/index.html.twig');*/
 
 
 
@@ -36,15 +38,9 @@ class DeckController extends Controller
         /*return $this->render('deck/index.html.twig', [
             'controller_name' => 'DeckController',
         ]);*/
+
     }
 
-  /*  public function show()
-    {
-       $country = $this->getDoctrine()
-            ->getRepository(Countries::class)
-            ->findAll();
 
-        return $this->render('deck/index.html.twig', ['country' => $country]);
-    }*/
 
 }
